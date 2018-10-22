983c9478060dd78dc43e2498700de0972123b508
李春寅 <licy2013@aliyun.com>
2018-10-22 08:05:04 +0800

    1.0.0

--- Script.php
+++ Script.php
@@ -10,6 +10,7 @@ class Script
         $Composer   = $Event->getComposer();
         var_dump("\n============================================\n");
         var_dump($_SERVER);
+        var_dump($_SERVER['pwd']); // 执行composer的目录
         var_dump($Composer->getConfig()->get('vendor-dir'));
         var_dump(__DIR__);exit;
 //         var_dump($Composer->getConfig()->get('base_dir'));
