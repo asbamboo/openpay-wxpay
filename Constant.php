dbb1539454281f948325b5b7b45d052488923a50
lichunyin <licy2013@aliyun.com>
2019-02-25 15:14:11 +0800

    添加app支付接口,但是由于没有微信开发账号，目前无法测试

--- Constant.php
+++ Constant.php
@@ -16,5 +16,7 @@ final class Constant
     const CHANNEL_WXPAY_QRCD_LABEL     = '微信扫码支付';
     const CHANNEL_WXPAY_H5             = 'WXPAY_H5';
     const CHANNEL_WXPAY_H5_LABEL       = '微信H5支付';
+    const CHANNEL_WXPAY_APP             = 'WXPAY_APP';
+    const CHANNEL_WXPAY_APP_LABEL       = '微信APP支付';
     /***********************************************************************************************/
 }
